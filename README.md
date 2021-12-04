e571788d14b03799e50940a22e0e1489f64e9025
Matthew Jacobs <codingmatty@gmail.com>
2021-12-03 22:45:04 -0500

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,68 +1,22 @@
-## Obsidian Sample Plugin
+# Obsidian Auto Linking Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This is an [Obsidian](https://obsidian.md/) plugin that makes it easy to convert common text patterns into links in previews.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+It's useful for things like Jira ticket labels or Github issues.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+Inspired by the similar feature in [Github's](github.com) [autolink reference feature](https://github.blog/2019-10-14-introducing-autolink-references/)
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+## Usage
 
-### First time developing plugins?
+1. Provide a label and click the `+` button.
+1. Provide a pattern, using standard [Javascript Regex](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions)
+1. Provide a link, using `{pattern}` to insert the matched pattern into the link.
 
-Quick starting guide for new plugin devs:
+## Example
 
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
-
-### Releasing new releases
-
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
-
-### Adding your plugin to the community plugin list
-
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-### How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-### Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-### Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-
-### API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+* Pattern: `JIRA-[\d]+`
+* Link: `https://team.atlassian.net/browse/{pattern}`
+* Will convert "JIRA-123" to a link in the markdown preview
+```
+<a href="https://team.atlassian.com/browse/JIRA-123" target="_blank">SHIP-123</a>
+```
--- main.ts
+++ main.ts
@@ -1,137 +1,132 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+type PluginSettings = Record<
+  string,
+  {
+    pattern: string;
+    link: string;
+  }
+>;
 
-interface MyPluginSettings {
-	mySetting: string;
+const DEFAULT_SETTINGS: PluginSettings = {};
+
+export default class AutoLinkingPlugin extends Plugin {
+  settings: PluginSettings;
+
+  async onload() {
+    await this.loadSettings();
+
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SettingTab(this.app, this));
+
+    this.registerMarkdownPostProcessor((el: HTMLElement) => {
+      Object.values(this.settings)
+        .filter(({ pattern, link }) => pattern && link)
+        .forEach(({ pattern, link }) => {
+          el.innerHTML = el.innerHTML.replace(
+            new RegExp(pattern, "g"),
+            (value) => {
+              const pattern = value.replace(/<[^>]*>/g, ""); // Remove any lingering HTML tags
+              return `<a href="${link.replace(
+                "{pattern}",
+                pattern
+              )}" target="_blank" class="external-link">${pattern}</a>`;
+            }
+          );
+        });
+    });
+  }
+
+  onunload() {}
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+class SettingTab extends PluginSettingTab {
+  plugin: AutoLinkingPlugin;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  constructor(app: App, plugin: AutoLinkingPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	async onload() {
-		await this.loadSettings();
+  display(): void {
+    const { containerEl } = this;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    containerEl.empty();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    containerEl.createEl("h2", { text: "Auto-Linking Settings" });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    let newSettingName = "";
+    new Setting(containerEl)
+      .setName("Add New Pattern")
+      .addText((text) => {
+        text.inputEl.style.marginRight = "6px";
+        text.setPlaceholder("Label").onChange((value) => {
+          newSettingName = value;
+        });
+      })
+      .addButton((button) => {
+        button.setButtonText("+").onClick(async () => {
+          if (!newSettingName) {
+            return;
+          }
+          this.plugin.settings = {
+            ...this.plugin.settings,
+            [newSettingName]: {
+              pattern: "",
+              link: "",
+            },
+          };
+          await this.plugin.saveSettings();
+          this.display();
+        });
+      });
+    Object.keys(this.plugin.settings).forEach((key) => {
+      new Setting(containerEl)
+        .setName(key)
+        .addText((text) => {
+          text.inputEl.style.marginRight = "6px";
+          text
+            .setPlaceholder("Pattern")
+            .setValue(this.plugin.settings[key].pattern)
+            .onChange(async (value) => {
+              this.plugin.settings[key].pattern = value;
+              await this.plugin.saveSettings();
+            });
+        })
+        .addText((text) => {
+          text.inputEl.style.marginRight = "6px";
+          text
+            .setPlaceholder("Link")
+            .setValue(this.plugin.settings[key].link)
+            .onChange(async (value) => {
+              this.plugin.settings[key].link = value;
+              await this.plugin.saveSettings();
+            });
+        })
+        .addButton((button) => {
+          button.setButtonText("-").onClick(async () => {
+            this.plugin.settings = Object.keys(this.plugin.settings)
+              .filter((settingsKey) => settingsKey !== key)
+              .reduce(
+                (settings, settingsKey) => ({
+                  ...settings,
+                  [settingsKey]: this.plugin.settings[settingsKey],
+                }),
+                {}
+              );
+            await this.plugin.saveSettings();
+            this.display();
+          });
+        });
+    });
+  }
 }
